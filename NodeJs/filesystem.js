be9871ba3fea4957bf3f9d2304c738557d393d5b
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2024-04-27 11:55:26 +0530

    Add files via upload

---
+++ NodeJs/filesystem.js
@@ -0,0 +1,54 @@
+    // to include file system we use require()
+var fs = require('fs')
+
+        // reading the file 
+// Three parameters - name of the file, type/format of the file/encoding(utf8), Callback
+
+fs.readFile('calculate.js','utf8',function(err,data){ 
+    console.log(data)
+})
+
+            // create file - appendFile(), open(), writeFile()
+
+        //Writing the file  - > fs.writeFile()
+//->replaces the specified file and content if it exists. If the file does not exist, a new file, containing the specified content, will be created
+
+fs.writeFile('calculate1.js','console.log("Wecome Aspire")',function(err,data){
+    if (err) throw err; 
+    console.log("Data Saved")
+})
+
+        // appending the file  -> fs.appendFile()
+//-> method appends specified content to a file. If the file does not exist, the file will be created
+
+fs.appendFile('calculate.js','console.log("done")',function(err,data){ 
+    console.log("Data Saved")
+})
+
+        // opening the file  -> fs.open(path,flag,callback)
+// -> flag is "w" for "writing", the specified file is opened for writing.
+fs.open('calculate1.js', 'w', function (err, file) {
+    if (err) throw err;
+    console.log('Saved!');
+});
+ 
+            // creating and deleting the file
+fs.writeFile('calculate2.js','console.log("done")',function(err,data){ 
+    console.log("we are going to delete this file")
+}) 
+            // deleting the file we use unlink() method
+
+fs.unlink('calculate2.js',function(err){
+    console.log("Deleted the file calc2")
+})
+
+            // creating and renaming the file
+fs.writeFile('calculate2.js','console.log("done")',function(err,data){ 
+    console.log("creating and renaming the file")
+}) 
+            // Rename the file
+fs.rename('calculate2.js', 'calcrenamedfile.js', function (err) {
+    if (err) throw err;
+        console.log('File Renamed!');
+});
+   
