1daedcdbe6dc11457e0c3ef314975056b443c65c
Sathiyabanu-R <113367395+Sathiyabanu-R@users.noreply.github.com>
2024-05-15 09:52:36 +0530

    Add files via upload

---
+++ routes/pages.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const userContoller = require("../controllers/users");
+router.get(["/", "/login"], (req, res) => {
+  //res.send("<h1>Hello Tutor Joes Salem</h1>");
+  res.render("login");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.get("/profile", userContoller.isLoggedIn, (req, res) => {
+   res.render("profile");
+  // if (req.user) {
+  //   res.render("profile", { user: req.user });
+  // } else {
+  //   res.redirect("/login");
+  // }
+});
+router.get("/home", userContoller.isLoggedIn, (req, res) => {
+  // console.log(req.name);
+  res.render("home");
+//   if (req.user) {
+//     res.render("home", { user: req.user });
+//   } else {
+//     res.redirect("/login");
+//   }
+});
+
+module.exports = router;
